85056a265d392f6660098520d1f710dbb5ed572b
Gabriel Oriente <gabriel.oriente@mirumagency.com>
2018-07-16 23:20:38 -0300

    Initial commit

---
+++ app.ts
@@ -0,0 +1,11 @@
+let message: string = "Help me, obi-wan Kenobi";
+console.log(message)
+
+let episode: number = 4
+console.log("this episode " + episode)
+episode = episode + 1
+console.log("Next episode is " + episode)
+
+let favoriteDroid
+favoriteDroid = 'bb-8'
+console.log("my favorite droid is " + favoriteDroid)
---
+++ dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me, obi-wan Kenobi";
+console.log(message);
+var episode = 4;
+console.log("this episode " + episode);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+var favoriteDroid;
+favoriteDroid = 'bb-8';
+console.log("my favorite droid is " + favoriteDroid);
