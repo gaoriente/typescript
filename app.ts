7dd7d20c0b3017261699c74effcc2cfe8316cc88
Gabriel Oriente <gabriel.oriente@mirumagency.com>
2018-07-18 15:10:07 -0300

    1.1

--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
-let message: string = "Help me, obi-wan Kenobi";
-console.log(message)
+import {Spacecraft, Containership} from './base-ships';
+import {MillenniumFalcon} from './starfighters';
 
-let episode: number = 4
-console.log("this episode " + episode)
-episode = episode + 1
-console.log("Next episode is " + episode)
+import * as _ from 'lodash'
+console.log(_.pad("typescript Examples", 40, "="))
 
-let favoriteDroid
-favoriteDroid = 'bb-8'
-console.log("my favorite droid is " + favoriteDroid)
+let ship = new Spacecraft('hyperdrive')
+ship.jumpIntoHyperspace()
+
+let falcon = new MillenniumFalcon()
+falcon.jumpIntoHyperspace()
+
+let goodForTheJob = ( ship: Containership ) => ship.cargoContainers > 2
+console.log(`Is falcon good for the job? ${goodForTheJob(falcon)? 'YES' : 'NO'} `)
